37732c8501b584f0a4498c270e29a1a55da17b18
Matthew Barlocker <matthew@lucidchart.com>
2013-07-31 07:20:26 -0600

    clean out prebuilt controllers, views, and tests. spaces to tabs.

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,17 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "Nark"
-  val appVersion      = "1.0-SNAPSHOT"
+	val appName         = "Nark"
+	val appVersion      = "1.0-SNAPSHOT"
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
-  )
+	val appDependencies = Seq(
+		jdbc,
+		anorm
+	)
 
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+	val main = play.Project(appName, appVersion, appDependencies).settings(
+		scalaVersion := "2.10.1"
+	)
 
 }
