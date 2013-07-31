266e6e4500f82b74335cce6dfcbcc2d9825acd09
Matthew Barlocker <matthew@lucidchart.com>
2013-07-31 16:49:24 -0600

    minor tweaks around page titles, graphite configuration, home page verbiage.

--- app/com/lucidchart/open/nark/controllers/Application.scala
+++ app/com/lucidchart/open/nark/controllers/Application.scala
@@ -2,9 +2,12 @@ package com.lucidchart.open.nark.controllers
 
 import play.api.mvc.Action
 
+import com.lucidchart.open.nark.request.AppAction
+import com.lucidchart.open.nark.views
+
 class Application extends AppController {
-	def index = Action {
-		Ok("hello")
+	def index = AppAction { implicit request =>
+		Ok(views.html.application.index())
 	}
 }
 
