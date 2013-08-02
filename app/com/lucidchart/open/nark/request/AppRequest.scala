e6411879d83f6b82240fc5d2c4b770aad1a8299c
Matthew Barlocker <matthew@lucidchart.com>
2013-08-02 15:12:59 -0600

    csrf hidden input helper

--- app/com/lucidchart/open/nark/request/AppRequest.scala
+++ app/com/lucidchart/open/nark/request/AppRequest.scala
@@ -6,12 +6,4 @@ import play.api.Play
 import play.api.Play.current
 
 class AppRequest[A](val request: Request[A]) extends WrappedRequest(request) {
-	def c = Play.configuration
-
-	def TOKEN_NAME: String = c.getString("csrf.token.name").getOrElse("csrf")
-	def COOKIE_NAME: Option[String] = c.getString("csrf.cookie.name") // If None, we search for TOKEN_NAME in play session
-
-	def tokenValue = {
-		request.cookies.get(TOKEN_NAME).map(_.value).getOrElse("")
-	}
 }
