30beefb5b2cd0a63f965d298a7da5201cfbacf46
Matthew Barlocker <matthew@lucidchart.com>
2014-07-25 12:45:39 -0600

    add pagerduty plugin

--- project/Build.scala
+++ project/Build.scala
@@ -19,7 +19,8 @@ object ApplicationBuild extends Build {
 		"org.apache.commons" % "commons-email" % "1.3",
 		"org.openid4java" % "openid4java" % "0.9.7",
 		"org.apache.httpcomponents" % "httpclient" % "4.2.5",
-		"com.lucidchart" %% "nark-plugin" % "1.0"
+		"com.lucidchart" %% "nark-plugin" % "1.0",
+		"com.lucidchart" %% "nark-pagerduty-plugin" % "1.0"
 	)
 
 	def writeToFile(fileName: String, value: String) = {
